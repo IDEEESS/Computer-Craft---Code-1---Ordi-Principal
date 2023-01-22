e3fae34a4a4d4ff2184778b19334f5a636da0ae7
IDEEESS <64473138+IDEEESS@users.noreply.github.com>
2023-01-22 14:26:02 +0100

    Update login.lua

--- login.lua
+++ login.lua
@@ -1,8 +1,7 @@
 user = "root"
 password = "IT"
 
-print(string.upper("Systeme Securite "))
-
+print(string.upper("Systeme Securite"))
 io.write("user :")
-userName = io.read
+local userName = io.read
 print("user = ",userName)
