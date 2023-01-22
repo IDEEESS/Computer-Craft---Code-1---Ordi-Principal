3263e90fcefdaccb7eaf618b9d77da3adbbcbd11
IDEEESS <64473138+IDEEESS@users.noreply.github.com>
2023-01-22 19:09:50 +0100

    Update login.lua

--- login.lua
+++ login.lua
@@ -1,7 +1,48 @@
 user = "root"
 password = "IT"
 
-print(string.upper("Systeme Securite"))
-io.write("user :")
-local userName = io.read
-print("user = ",userName)
+redstone.setOutput("back",false)
+
+function login()
+	print(string.upper("Systeme Securite"))
+	io.write("user : ")
+	local userName = io.read()
+	io.write("password : ")
+	local passwordPass = io.read()
+	
+	if user == userName then 
+		
+		if password == passwordPass then
+		
+			redstone.setOutput("back",true)
+			term.setBackgroundColor(colors.green)
+			print("La porte est ouverte , vous pouvez y aller")
+			sleep(3)
+			redstone.setOutput("back",false)
+			shell.exit()
+			term.clear()
+			term.setCursorPos(1,1)
+		else
+			term.setBackgroundColor(colors.red)
+			print("Mauvais !")
+			term.setBackgroundColor(colors.black)
+			shell.exit()
+			term.clear()
+			term.setCursorPos(1,1)
+		end		
+
+	else
+		term.setBackgroundColor(colors.red)
+		print("Mauvais !")
+		term.setBackgroundColor(colors.black)
+		shell.exit()
+		term.clear()
+		term.setCursorPos(1,1)
+
+	end
+
+	login()
+end
+
+
+login()
